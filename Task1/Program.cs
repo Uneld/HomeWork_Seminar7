f7938aacb950a59af725bbb2f064c24ed9cc3027
Uneld <aleksey.minchenya@gmail.com>
2023-03-05 15:41:56 +0300

    added solving the task1

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿//HomeWork Task 1
+Console.Clear();
+
+System.Console.Write("Please enter number 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Please enter number 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"{a} > {b}");
+}
+else if (a < b)
+{
+    Console.WriteLine($"{a} < {b}");
+}
+else
+{
+    Console.WriteLine($"{a} = {b}");
+}
