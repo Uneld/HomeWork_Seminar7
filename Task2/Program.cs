36d9247d087ca1563b9f9c539325b79bb6fef778
Uneld <aleksey.minchenya@gmail.com>
2023-03-05 15:51:35 +0300

    added solving the task2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿//HomeWork Task 2
+Console.Clear();
+
+System.Console.Write("Please enter number 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Please enter number 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Please enter number 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+
+Console.WriteLine($"Max of numbers = {max}");
