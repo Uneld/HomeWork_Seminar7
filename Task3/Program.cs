37040949825d9c097388e38f804622e255fe5708
Uneld <aleksey.minchenya@gmail.com>
2023-03-05 15:57:23 +0300

    added solving the task3

---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿//HomeWork Task 3
+Console.Clear();
+
+System.Console.Write("Please enter number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 != 0)
+{
+    Console.WriteLine($"Number {a} is odd"); //нечетное
+}
+else
+{
+    Console.WriteLine($"Number {a} is even"); //четное
+}
+
+
+
